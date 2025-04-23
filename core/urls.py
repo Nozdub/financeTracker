823574f8481e8c38ce7aa39d90ad3f21c0c2d1a9
Christian <chriaas@hiof.no>
2025-04-23 16:02:05 +0200

    adds register user option as well as login, also sorted out user filtered transactions.

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from finance.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finance.urls')),
+    path('register/', register, name='register'),
 ]
--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('transaction/', views.transaction_history, name='transaction_history'),
+    path('transactions/', views.transaction_history, name='transaction_history'),
+    path('login/', auth_views.LoginView.as_view(template_name='finance/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
